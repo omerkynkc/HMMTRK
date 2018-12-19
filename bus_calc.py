9e8053ea63ca5d9fb922e450909c57c1f279d204
TheLastEtlekmek <eklekmek07@gmail.com>
2018-12-20 01:07:35 +0300

    working relase

--- bus_calc.py
+++ bus_calc.py
@@ -1,3 +1,4 @@
+#fix which days error
 import time
 from math import floor
 #work in progres.. the program that shows how many minutes to next coming rk for Sarıtepe Campus
@@ -6,7 +7,14 @@ weekday = ("07.00", "08.00", "09.00", "09.55", "10.35", "12.15", "13.05", "13.50
 saturday = ("07.20", "08.45", "10.00", "11.10", "12.25", "13.40", "15.00", "16.15", "17.45", "19.15", "20.35", "21.50", "23.55")
 sunday = ("08.45", "11.15", "13.45", "16.15", "19.15", "19.45", "21.50","22.15", "23.55")
 
-def calc(day):
+def calc():
+    cd = time.strftime("%c")
+    if cd == "Sun":
+        day = sunday
+    elif cd == "Sat":
+        day = saturday
+    else:
+        day = weekday
     cm = time.strftime("%M")
     ch = time.strftime("%H")
     cs = time.strftime("%S") 
@@ -22,17 +30,5 @@ def calc(day):
         if sub > 0:
             break
         count += 1
-    print("Next Bus: {}".format(day[count]))
-    print("{} remaning".format(sub))
-    print(round(sub//3600),floor(sub/60%60))
-cd = 1
-if cd == "Sun":
-    calc(sunday)
-elif cd == "Sat":
-    calc(saturday)
-else:
-    calc(weekday)
+    return ("{}.{} remaing to 59RK".format(round(sub//3600),floor(sub/60%60)))
 
-for i in range(99):
-    calc(weekday)
-    time.sleep(10)
